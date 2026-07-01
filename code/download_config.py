8b1f2e735b1d644241959722f95c645a8e7cdffd
miriam <7304202+zentouro@users.noreply.github.com>
2026-07-01 11:44:08 -0400

    Update environment yaml, reorganize landmask files
    environment and yaml should now allow 'cmip6_file_check.ipynb' to run

--- code/download_config.py
+++ code/download_config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # local paths (imput your own)
 lpaths = {
-	"raw_data_dir":"/Users/miriam/Documents/02_academia/02_PhD/research/projects/dirt_in_sesa/CMIP6_data/"
+	"raw_data_dir":"/Users/miriam/Documents/02_academia/02_PhD/research/projects/dirt_in_sesa/CMIP6_data/",
+    "landmask_dir":"/Users/miriam/Documents/02_academia/02_PhD/research/projects/dirt_in_sesa/landmasks/"
 }
